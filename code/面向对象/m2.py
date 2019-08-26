fea6bf0e08df9f57ca70b4db5d205043a7eaa231
RemainderTime <remaindertime@gmail.com>
2019-08-26 22:36:04 +0800

    2019-8-26

--- code/面向对象/m2.py
+++ code/面向对象/m2.py
@@ -6,13 +6,43 @@
 #构造函数
 
 class Student():
-    name = '77'
-
+    name = '77'   #类变量
+    sum = 5
+    source=0
+ 
+     #构造方法
     def __init__(self,name):
         self.name=name
+        self.source= 0
+        print(name) #调用实例变量 方式1
+        print(self.name) #调用实例变量 方式2
+        print(self.__class__.name)#调用类变量 方式1
 
-    def show(self):
+    #实例方法
+    def show(self):  
         print("python")
 
-student = Student('哈哈')
+    #类方法
+    @classmethod  
+    def plus_sum(cls):
+        cls.sum +=1  #调用类变量
+        print(cls.sum)
+
+    #安全赋值
+    def marking(self,source):
+        if source<0:
+            self.source=0
+        self.source = source
+        print('分数：'+str(self.source))
+
+
+
+
+student = Student('哈哈')#实例变量
 print(student.name)
+
+
+#调用类变量 方式2
+print(Student.name)  
+
+student.marking(22)
